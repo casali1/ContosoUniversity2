17acd6a0b6d2b82eefc0bb400f439cf869d61a15
casali1 <frank_casali@hotmail.com>
2019-10-06 23:24:00 -0600

    Added functionality to Details page.

--- ContosoUniversity2/Controllers/StudentsController.cs
+++ ContosoUniversity2/Controllers/StudentsController.cs
@@ -30,7 +30,11 @@ namespace ContosoUniversity2.Controllers
         {
             if (id == null) return NotFound();
 
-            var student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+            var student = await _context.Students
+                            .Include(s => s.Enrollments)
+                                .ThenInclude(e => e.Course)
+                            .AsNoTracking()
+                            .FirstOrDefaultAsync(m => m.ID == id);
 
             if (student == null) return NotFound();
 
@@ -48,13 +52,23 @@ namespace ContosoUniversity2.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        public async Task<IActionResult> Create([Bind("EnrollmentDate,FirstMidName,LastName")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
             }
             return View(student);
         }
