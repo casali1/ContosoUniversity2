f51c0027d739c90386aa6d142df3d4afd92c3f76
casali1 <frank_casali@hotmail.com>
2019-10-08 16:42:58 -0600

    More changes.

--- ContosoUniversity2/Controllers/HomeController.cs
+++ ContosoUniversity2/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversity2.Models;
+using ContosoUniversity2.Data;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity2.Models.SchoolViewModels;
 
 namespace ContosoUniversity2.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -24,6 +32,19 @@ namespace ContosoUniversity2.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
---
+++ Tests/ControllerTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContosoUniversity2.Data;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitControllerTestsTest1
+    {
+        [TestMethod]
+        public void TestContext()
+        {
+
+
+        }
+    }
+}
